c016ea6794199c8cd340cd84ddc619f870790219
Chris "Mo" Mochinski <cmochinski@gmail.com>
2021-09-23 16:38:18 -0500

    started on CustomerInfo component

---
+++ src/components/CustomerInfo/CustomerInfo.jsx
@@ -0,0 +1,12 @@
+import { useDispatch } from 'react-redux';
+import { useState } from 'react-redux';
+
+function CustomerInfo() {
+
+    const [newCustomer, setNewCustomer] = useState([]);
+
+    
+
+}
+
+export default CustomerInfo;
