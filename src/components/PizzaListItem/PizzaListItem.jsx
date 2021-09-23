3c95453c71a6588de0bc8eed7e2eead1fc112938
Chris "Mo" Mochinski <cmochinski@gmail.com>
2021-09-23 14:13:14 -0500

    PizzaListItembranch rough draft

---
+++ src/components/PizzaListItem/PizzaListItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+//this function/component will send each individual pizza to the map loop in PizzaList
+//TODO - add the conditional rendering for add/remove button(s)
+
+function PizzaListItem({ pizza }) {
+    return (
+        <div className="container">
+            <li key={index}>
+                <img width="200px" height="200px" src={pizza.image_path} />
+                {pizza.name} -
+                {pizza.description} -
+                {pizza.price}
+            </li>
+        </div>
+    )
+}
+
+export default PizzaListItem;
