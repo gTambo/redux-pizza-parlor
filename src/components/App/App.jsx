416cdacab020b5741cf3fd7994cdffa4bd2cfc2e
Phaydara Vongsavanthong <stephen.mussel@gmail.com>
2021-09-23 16:07:42 -0500

    moved header into component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import Header from '../Header/Header';
 
 function App() {
 
   return (
 
     <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Prime Pizza</h1>
-      </header>
+      <Header />
       <Router>
         <ul>
           <li>
