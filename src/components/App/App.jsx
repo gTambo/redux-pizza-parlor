287b47c91b18156ca41bd824278be314bb394cf7
Sarah Helgen <sarah.helgen@gmail.com>
2021-09-23 14:09:22 -0500

    added routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import {HashRouter as Router, Route } from 'react-router-dom';
 
 function App() {
 
   return (
+  
+
+
+      
+  
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
       </header>
+      <Router>
+
+        <Route>
+          <CustomerInfo />
+        </Route>
+
+        <Route>
+          <Checkout />
+        </Route>
+
+        <Route>
+          <SelectPizza />
+        </Route>
+
+        <Route>
+          <Admin />
+        </Route>
+
+
+        </Router>
   
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
